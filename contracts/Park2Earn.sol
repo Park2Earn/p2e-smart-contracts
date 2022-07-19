f50228ac783974d60976117817e71cf7b2d57420
rimatik <slaven@rimatik.hr>
2022-07-19 16:29:26 +0200

    added staking, withdrawing and tests for them

--- contracts/Park2Earn.sol
+++ contracts/Park2Earn.sol
@@ -7,78 +7,151 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Park2Earn is Ownable {
- using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    struct Promotion {
-        address creator;
-        IERC20 token;
-        uint256 createdAt;
-        uint256 startTime;
-        uint256 promoLength;
-    }
+  struct Promotion {
+    address creator;
+    IERC20 token;
+    uint256 createdAt;
+    uint256 startTime;
+    uint256 promoLength;
+  }
 
-    struct PrivateGood {
-        address creator;
-        uint256 createdAt;
-        address recipient;
-    }
+  struct PrivateGood {
+    address creator;
+    uint256 createdAt;
+    address recipient;
+  }
 
-    event CreatePromotion(uint256 promotionId);
-    event CreatePrivateGood(uint256 promotionId);
+  struct Staker {
+    uint256 amount;
+    uint256 promotionId;
+  }
 
-    mapping (uint256 => Promotion) _promotions;
-    uint256 internal _latestPromotionId;
+  event CreatePromotion(uint256 promotionId);
+  event CreatePrivateGood(uint256 privateGoodId);
+  event StakedTokens(address staker, uint256 amount, uint256 promotionId);
+  event WithdrawnTokens(address staker, uint256 amount);
 
-    mapping (uint256 => PrivateGood) _privateGoods;
-    uint256 internal _latestPrivateGoodId;
+  mapping(uint256 => Promotion) private _promotions;
+  uint256 internal _latestPromotionId;
 
+  mapping(uint256 => PrivateGood) private _privateGoods;
+  uint256 internal _latestPrivateGoodId;
 
-    function createPromotion(IERC20 token, uint256 startTime, uint256 length) public onlyOwner{
+  //mapping between promotion and user staked
+  mapping(address => Staker) private _stakes;
 
-        require(address(token) != address(0), "Can't be zero address");
-        require(startTime > 0, "Start time must be positive");
-        require(length > 0, "Lenght must be positive");
+  function createPromotion(
+    address token,
+    uint256 startTime,
+    uint256 length
+  ) public onlyOwner {
+    require(address(token) != address(0), "Can't be zero address");
+    require(startTime > 0, "Start time must be positive");
+    require(length > 0, "Lenght must be positive");
 
-        uint256 nextPromotionId = _latestPromotionId.add(1);
-        _latestPromotionId = nextPromotionId;
-        
-        _promotions[nextPromotionId] = Promotion({
-            creator: msg.sender,
-            token: token,
-            createdAt: block.timestamp,
-            startTime: startTime,
-            promoLength: length
-        });
+    uint256 nextPromotionId = _latestPromotionId.add(1);
+    _latestPromotionId = nextPromotionId;
 
-        emit CreatePromotion(nextPromotionId);
-    }
+    _promotions[nextPromotionId] = Promotion({
+      creator: msg.sender,
+      token: IERC20(token),
+      createdAt: block.timestamp,
+      startTime: startTime,
+      promoLength: length
+    });
 
-    function getPromotion(uint256 id) public view returns(Promotion memory){
-        Promotion memory promotion = _promotions[id];
-        return promotion;
-    }
+    emit CreatePromotion(nextPromotionId);
+  }
 
-     function createPrivateGood(address recipient) public onlyOwner{
+  function getPromotion(uint256 id) public view returns (Promotion memory) {
+    Promotion memory promotion = _promotions[id];
+    return promotion;
+  }
 
-        require(recipient != address(0), "Can't be zero address");
+  function getPromotionToken(uint256 id) public view returns (IERC20 token) {
+    token = getPromotion(id).token;
+  }
 
-        uint256 nextPrivateGoodId = _latestPrivateGoodId.add(1);
-        _latestPrivateGoodId = nextPrivateGoodId;
-        
-        _privateGoods[nextPrivateGoodId] = PrivateGood({
-            creator: msg.sender,
-            createdAt: block.timestamp,
-            recipient: recipient
-        });
+  function getLatestPromotion() public view returns (PrivateGood memory) {
+    return getPrivateGood(_latestPromotionId);
+  }
 
-        emit CreatePrivateGood(_latestPrivateGoodId);
-    }
+  function isPromotionExpired(uint256 id) public view returns (bool isExpired) {
+    uint256 endTime = _promotions[id].startTime.add(
+      _promotions[id].promoLength
+    );
+    isExpired = endTime > block.timestamp;
+  }
 
-    function getPrivateGood(uint256 id) public view returns(PrivateGood memory){
-        PrivateGood memory privateGood = _privateGoods[id];
-        return privateGood;
-    }
+  function isPromotionToken(address token, uint256 promotionId)
+    public
+    view
+    returns (bool)
+  {
+    return _promotions[promotionId].token == IERC20(token);
+  }
+
+  function createPrivateGood(address recipient) public onlyOwner {
+    require(recipient != address(0), "Can't be zero address");
+
+    uint256 nextPrivateGoodId = _latestPrivateGoodId.add(1);
+    _latestPrivateGoodId = nextPrivateGoodId;
+
+    _privateGoods[nextPrivateGoodId] = PrivateGood({
+      creator: msg.sender,
+      createdAt: block.timestamp,
+      recipient: recipient
+    });
+
+    emit CreatePrivateGood(_latestPrivateGoodId);
+  }
+
+  function getPrivateGood(uint256 id) public view returns (PrivateGood memory) {
+    PrivateGood memory privateGood = _privateGoods[id];
+    return privateGood;
+  }
+
+  function stake(
+    address token,
+    uint256 amount,
+    uint256 promotionId
+  ) public {
+    require(isPromotionExpired(promotionId), "Promotion expired!");
+    require(isPromotionToken(token, promotionId), "Not promotion token");
+    require(amount > 0, "Invalid amount");
+
+    _stakes[msg.sender].amount = _stakes[msg.sender].amount.add(amount);
+
+    IERC20(token).transferFrom(msg.sender, address(this), amount);
+    emit StakedTokens(msg.sender, amount, promotionId);
+  }
+
+  function withdraw(uint256 amount, uint256 promotionId) public {
+    require(!isPromotionExpired(promotionId), "Promotion still running!");
+    require(amount > 0, "Invalid amount!");
+    require(_stakes[msg.sender].amount >= amount, "Not enough staked!");
+    require(_stakes[msg.sender].amount > 0, "Nothing staked!");
+
+    IERC20 token = getPromotionToken(promotionId);
+    require(token.balanceOf(address(this)) >= amount, "Not enough balance!");
+
+    _stakes[msg.sender].amount = _stakes[msg.sender].amount.sub(amount);
+
+    IERC20(token).approve(address(this), amount);
+    IERC20(token).transferFrom(address(this), msg.sender, amount);
+    emit WithdrawnTokens(msg.sender, amount);
+  }
+
+  function getAmountStaked(address staker)
+    public
+    view
+    returns (uint256 amount)
+  {
+    amount = _stakes[staker].amount;
+  }
 }
--- test/park2Earn.test.ts
+++ test/park2Earn.test.ts
@@ -1,51 +1,228 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { ContractFactory } from "ethers";
+import { Contract, ContractFactory } from "ethers";
 import { ethers } from "hardhat";
 import { Park2Earn } from "../typechain-types";
+import {
+  MockContract,
+  MockContractFactory,
+  smock,
+} from "@defi-wonderland/smock";
+import { getAddress } from "ethers/lib/utils";
 
 describe("Park2Earn tests", function () {
   let alice: SignerWithAddress;
+  let bob: SignerWithAddress;
 
-  let gooddeedToken: ContractFactory;
   let park2EarnFactory: ContractFactory;
-  let gooddeedTokenAddress: any;
+  let erc20Factory: MockContractFactory<ContractFactory>;
+  let usdc: MockContract<Contract>;
+  let usdt: MockContract<Contract>;
 
   let park2EarnContract: Park2Earn;
 
   const _start = Math.round(Date.now() / 1000);
-  const length = 60 * 60 * 8;
+  const promoLength = 60 * 60 * 8;
+  // Initial mint 10,000
+  const initialMint = "10000000000000000000000";
+  // Staking amount 1000
+  const stakingAmount = "1000000000000000000000";
 
   before(async () => {
-    [alice] = await ethers.getSigners();
-    gooddeedToken = await ethers.getContractFactory("GooddeedToken");
+    [alice, bob] = await ethers.getSigners();
     park2EarnFactory = await ethers.getContractFactory("Park2Earn");
+    erc20Factory = await smock.mock("MockERC20");
   });
 
   beforeEach(async () => {
-    const gooddeedTokenContract = await gooddeedToken.deploy();
-    gooddeedTokenAddress = gooddeedTokenContract.address;
-
     park2EarnContract = (await park2EarnFactory.deploy()) as Park2Earn;
+
+    usdc = await erc20Factory.deploy("Usdc token", "USDC", 18);
+    usdt = await erc20Factory.deploy("Usdt token", "USDT", 18);
+    await usdc.mint(bob.address, initialMint);
   });
 
   it("Should create and get promotion", async function () {
-    await park2EarnContract.createPromotion(
-      gooddeedTokenAddress,
-      _start,
-      length
-    );
-
+    await park2EarnContract.createPromotion(usdc.address, _start, promoLength);
     const getPromo = await park2EarnContract.getPromotion(1);
 
-    expect(getPromo.promoLength).to.equal(length);
+    expect(getPromo.promoLength).to.equal(promoLength);
+    expect(getAddress(getPromo.token)).to.equal(usdc.address);
   });
 
   it("Should create and get private good", async function () {
     await park2EarnContract.createPrivateGood(alice.address);
-
     const privateGood = await park2EarnContract.getPrivateGood(1);
 
     expect(privateGood.recipient).to.equal(alice.address);
+  });
+
+  it("Should stake correct amount", async function () {
+    await park2EarnContract.createPromotion(usdc.address, _start, promoLength);
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+    await park2EarnContract.connect(bob).stake(usdc.address, stakingAmount, 1);
+
+    const amountStaked = await park2EarnContract.getAmountStaked(bob.address);
+
+    expect(stakingAmount).to.equal(amountStaked);
+  });
+
+  it("Should not stake if promotion expired", async function () {
+    await park2EarnContract.createPromotion(usdc.address, _start, promoLength);
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await ethers.provider.send("evm_increaseTime", [promoLength]);
+    await ethers.provider.send("evm_mine", [timestampBefore + promoLength]);
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await expect(
+      park2EarnContract.connect(bob).stake(usdc.address, stakingAmount, 1)
+    ).to.be.revertedWith("Promotion expired!");
+  });
+
+  it("Should not stake if wrong promotion token", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await expect(
+      park2EarnContract.connect(bob).stake(usdt.address, stakingAmount, 1)
+    ).to.be.revertedWith("Not promotion token");
+  });
+
+  it("Should not stake if amount zero", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await expect(
+      park2EarnContract.connect(bob).stake(usdc.address, 0, 1)
+    ).to.be.revertedWith("Invalid amount");
+  });
+
+  it("Should revert if promotion still running", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await park2EarnContract.connect(bob).stake(usdc.address, stakingAmount, 1);
+
+    await expect(
+      park2EarnContract.connect(bob).withdraw(stakingAmount, 1)
+    ).to.be.revertedWith("Promotion still running!");
+  });
+
+  it("Should revert if amount is zero", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await park2EarnContract.connect(bob).stake(usdc.address, stakingAmount, 1);
+
+    await ethers.provider.send("evm_increaseTime", [promoLength]);
+    await ethers.provider.send("evm_mine", [timestampBefore + promoLength]);
+
+    await expect(
+      park2EarnContract.connect(bob).withdraw(0, 1)
+    ).to.be.revertedWith("Invalid amount!");
+  });
+  it("Should revert if not enough staked", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await park2EarnContract.connect(bob).stake(usdc.address, stakingAmount, 1);
+
+    await ethers.provider.send("evm_increaseTime", [promoLength]);
+    await ethers.provider.send("evm_mine", [timestampBefore + promoLength]);
+
+    await expect(
+      park2EarnContract.connect(bob).withdraw(stakingAmount + 1, 1)
+    ).to.be.revertedWith("Not enough staked!");
+  });
+
+  it("Should revert if nothing staked", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await ethers.provider.send("evm_increaseTime", [promoLength]);
+    await ethers.provider.send("evm_mine", [timestampBefore + promoLength]);
+
+    await expect(
+      park2EarnContract.connect(bob).withdraw(stakingAmount, 1)
+    ).to.be.revertedWith("Nothing staked!");
+  });
+
+  it("Should withdraw tokens", async function () {
+    const blockNumBefore = await ethers.provider.getBlockNumber();
+    const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+    const timestampBefore = blockBefore.timestamp;
+
+    await park2EarnContract.createPromotion(
+      usdc.address,
+      timestampBefore,
+      promoLength
+    );
+
+    await usdc.connect(bob).approve(park2EarnContract.address, stakingAmount);
+
+    await park2EarnContract.connect(bob).stake(usdc.address, stakingAmount, 1);
+
+    await ethers.provider.send("evm_increaseTime", [promoLength]);
+    await ethers.provider.send("evm_mine", [timestampBefore + promoLength]);
+
+    await park2EarnContract.connect(bob).withdraw(stakingAmount, 1);
+
+    expect(await park2EarnContract.getAmountStaked(bob.address)).to.be.equal(0);
   });
 });
